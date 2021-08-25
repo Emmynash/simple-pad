889bb4ceabeb25071b1306c4f960ae72b86d264b
Akita Emmanuel <e.akita91@gmail.com>
2021-08-25 10:44:04 +0100

    Add Component(): MainPanel

---
+++ src/components/MainPanel.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import clsx from 'clsx'
+import { makeStyles, createStyles } from "@material-ui/core";
+import { IntrinsicElements } from "react-markdown/src/ast-to-react";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      flexShrink: 1,
+      flexBasis: '100%',
+      overflowY: 'auto',
+      position: 'relative',
+      zIndex: 0,
+      padding: theme.spacing(4)
+    }
+  }))
+interface props extends Partial<IntrinsicElements['div']>{
+  className?: string
+}
+export const MainPanel = React.forwardRef<HTMLDivElement, props>(
+  ({ className, children, ...rest }, ref) => {
+    const classes = useStyles();
+    return (
+      <div className={clsx(classes.root, className)} ref={ref} {...rest}>
+        {children}
+      </div>
+    )
+    }
+  )
