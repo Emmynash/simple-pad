ca0fe5e1761c1e796b52c98aa45b13c371332b11
Akita Emmanuel <e.akita91@gmail.com>
2021-08-28 17:19:57 +0100

    Added():  NoteTextField

---
+++ src/components/NoteTextField.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import clsx from 'clsx'
+import {
+  createStyles,
+  makeStyles,
+  TextField,
+  OutlinedTextFieldProps
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    inp: {
+      marginBottom: theme.spacing(3)
+    }
+  })
+)
+
+export const NoteTextField = ({
+  className,
+  value,
+  name,
+  ...rest
+}: Partial<OutlinedTextFieldProps>) => {
+  const classes = useStyles();
+  return (
+    <TextField
+      id={name}
+      name={name}
+      variant='outlined'
+      fullWidth
+      autoComplete='off'
+      className={clsx(classes.inp, className)}
+      InputLabelProps={{
+        shrink: Boolean(value)
+      }}
+      value={value}
+      {...rest}
+    />
+  )
+}
